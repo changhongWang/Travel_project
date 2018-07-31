6c3902852125b24097902c071b0e971ffe3237c6
changhong <changhongwang@126.com>
2018-07-31 14:11:08 +0800

    index-icon finished

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="icons">
+    <swiper>
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="icon of page" :key="icon.id">
+          <div class="icon-img-box">
+            <img :src="icon.imgUrl" alt="" class="icon-img">
+          </div>
+          <div class="icon-desc">{{icon.desc}}</div>
+        </div>
+      </swiper-slide>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons',
+  data () {
+    return {
+      icons: [
+        {
+          id: '001',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png',
+          desc: '热门景点'
+        },
+        {
+          id: '002',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/ff/fdf170ee89594b02.png',
+          desc: '北京必游'
+        },
+        {
+          id: '003',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6a/45f595250c73d102.png',
+          desc: '夏日玩水'
+        },
+        {
+          id: '004',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/20/831d62d2e1c7be02.png',
+          desc: '文化古迹'
+        },
+        {
+          id: '005',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/76/eb88861d78fb9902.png',
+          desc: '动植物园'
+        },
+        {
+          id: '006',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/6c/9e54a8540fee0102.png',
+          desc: '故宫'
+        },
+        {
+          id: '007',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/5a/13ceb38dcf262f02.png',
+          desc: '一日游'
+        },
+        {
+          id: '008',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/95/8d02011d149bdb02.png',
+          desc: '汽车票'
+        },
+        {
+          id: '009',
+          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/95/8d02011d149bdb02.png',
+          desc: '汽车票'
+        }
+      ]
+    }
+  },
+  computed: {
+    pages () {
+      const pages = []
+      this.icons.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+        if (!pages[page]) {
+          pages[page] = []
+        }
+        pages[page].push(item)
+      })
+      return pages
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  @import '~styles/mixins.styl'
+  .icons
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    .icon
+      overflow: hidden
+      position: relative
+      float: left
+      padding-bottom: 25%
+      height: 0
+      width: 25%
+      .icon-img-box
+        position: absolute
+        top: 0
+        right: 0
+        left: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        .icon-img
+          display: block
+          margin: 0 auto
+          height: 100%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        text-align: center
+        color: $darkTextColor
+        ellipsis()
+</style>

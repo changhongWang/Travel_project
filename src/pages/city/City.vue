24670aeab78cb96ff532eac8065b6fd12fa5740f
changhong <changhongwang@126.com>
2018-08-01 11:52:16 +0800

    base function of City.vue completed

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,18 +1,50 @@
 <template>
   <div>
-   <city-header></city-header>
+    <city-header></city-header>
     <city-input></city-input>
+    <city-list :hot="hotCities" :cities="cities" :letter="clickedLetter"></city-list>
+    <city-alphabet :cities="cities" @letterClick="handleLetterClick"></city-alphabet>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 import CityHeader from './components/Header'
 import CityInput from './components/Input'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
 export default {
   name: 'City',
+  mounted () {
+    this.getCityInfo()
+  },
+  data () {
+    return {
+      hotCities: [],
+      cities: [],
+      clickedLetter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json').then(this.getCityInfoSuccess)
+    },
+    getCityInfoSuccess (res) {
+      if (res.data.ret && res.data.data) {
+        const data = res.data.data
+        this.hotCities = data.hotCities
+        this.cities = data.cities
+      }
+    },
+    handleLetterClick (letter) {
+      this.clickedLetter = letter
+    }
+  },
   components: {
     CityHeader,
-    CityInput
+    CityInput,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,80 @@
+<template>
+  <ul class="list">
+    <li class="item" v-for="item in letters"
+        :key="item"
+        :ref="item"
+        @click="handleLetterClick"
+        @touchstart="handleTouchStart"
+        @touchmove="handleTouchMove"
+        @touchend="handleTouchEnd"
+    >{{item}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet',
+  props: ['cities'],
+  data () {
+    return {
+      touchStatus: false,
+      startY: 0,
+      timer: null
+    }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (let i in this.cities) {
+        letters.push(i)
+      }
+      return letters
+    }
+  },
+  methods: {
+    handleLetterClick (e) {
+      this.$emit('letterClick', e.target.innerText)
+    },
+    handleTouchStart () {
+      this.touchStatus = true
+    },
+    handleTouchMove (e) {
+      if (this.touchStatus) {
+        if (this.timer) {
+          clearTimeout(this.timer)
+        }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 17)
+          if (index >= 0 && index <= this.letters.length) {
+            this.$emit('letterClick', this.letters[index])
+          }
+        }, 16)
+      }
+    },
+    handleTouchEnd () {
+      this.touchStatus = false
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variables.styl'
+  .list
+    position: absolute
+    top: 1.58rem
+    right: 0
+    bottom: 0
+    width: .4rem
+    display: flex
+    flex-direction: column
+    justify-content: center
+    .item
+      line-height: .35rem
+      color: $bgColor
+      text-align: center
+</style>
